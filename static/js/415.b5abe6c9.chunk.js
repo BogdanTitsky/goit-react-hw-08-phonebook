c4e9dfd9b0d4a85889aade5ca86922c8086dc8b1
BogdanTitsky <119936698+BogdanTitsky@users.noreply.github.com>
2023-07-17 14:49:47 +0000

    Deploying to gh-pages from @ BogdanTitsky/goit-react-hw-08-phonebook@30d41ed9a5e00f50454c244df37a7a0063e40cc1 🚀

---
+++ static/js/415.b5abe6c9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{415:function(e,n,t){t.r(n),t.d(n,{default:function(){return a}});var c=t(184);function a(){return(0,c.jsx)("section",{children:(0,c.jsx)("div",{className:"container container-content",children:(0,c.jsxs)("h1",{className:"h",children:["Contacts book welcome page",(0,c.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})})})}}}]);
+//# sourceMappingURL=415.b5abe6c9.chunk.js.map
